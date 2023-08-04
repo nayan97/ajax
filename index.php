d73821ee5ea1ba67fb938e231971d543d889a5ac
nayan97 <nayanislam085@gmail.com>
2023-08-04 23:35:27 +0600

    ajax basic

---
+++ index.php
@@ -0,0 +1,209 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Development Area</title>
+	<!-- ALL CSS FILES  -->
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/style.css">
+	<link rel="stylesheet" href="assets/css/responsive.css">
+</head>
+<body>
+	<div class="menu m-5">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<a class="bnt btn-sm btn-primary" href="index.php">Home</a>
+					<a class="bnt btn-sm btn-primary" href="create.php">Add</a>
+					<a class="bnt btn-sm btn-primary" href="profile.php">Profile</a>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="app">
+		<div class="wrap-table shadow">
+			<div class="card">
+				<div class="card-body">
+					<h2>All Data</h2>
+					<table class="table table-striped">
+						<thead>
+							<tr>
+								<th>#</th>
+								<th>Name</th>
+								<th>Email</th>
+								<th>Cell</th>
+								<th>Photo</th>
+								<th>Action</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>1</td>
+								<td>Asraful Haque</td>
+								<td>haq@gmail.com</td>
+								<td>01717700811</td>
+								<td><img src="assets/media/img/pp_photo/istockphoto-615279718-612x612.jpg" alt=""></td>
+								<td>
+									<a class="btn btn-sm btn-info" href="#">View</a>
+									<a class="btn btn-sm btn-warning" href="#">Edit</a>
+									<a class="btn btn-sm btn-danger" href="#">Delete</a>
+								</td>
+							</tr>
+							<tr>
+								<td>1</td>
+								<td>Asraful Haque</td>
+								<td>haq@gmail.com</td>
+								<td>01717700811</td>
+								<td><img src="assets/media/img/pp_photo/istockphoto-615279718-612x612.jpg" alt=""></td>
+								<td>
+									<a class="btn btn-sm btn-info" href="#">View</a>
+									<a class="btn btn-sm btn-warning" href="#">Edit</a>
+									<a class="btn btn-sm btn-danger" href="#">Delete</a>
+								</td>
+							</tr>
+							<tr>
+								<td>1</td>
+								<td>Asraful Haque</td>
+								<td>haq@gmail.com</td>
+								<td>01717700811</td>
+								<td><img src="assets/media/img/pp_photo/istockphoto-615279718-612x612.jpg" alt=""></td>
+								<td>
+									<a class="btn btn-sm btn-info" href="#">View</a>
+									<a class="btn btn-sm btn-warning" href="#">Edit</a>
+									<a class="btn btn-sm btn-danger" href="#">Delete</a>
+								</td>
+							</tr>
+							<tr>
+								<td>1</td>
+								<td>Asraful Haque</td>
+								<td>haq@gmail.com</td>
+								<td>01717700811</td>
+								<td><img src="assets/media/img/pp_photo/istockphoto-615279718-612x612.jpg" alt=""></td>
+								<td>
+									<a class="btn btn-sm btn-info" href="#">View</a>
+									<a class="btn btn-sm btn-warning" href="#">Edit</a>
+									<a class="btn btn-sm btn-danger" href="#">Delete</a>
+								</td>
+							</tr>
+							<tr>
+								<td>1</td>
+								<td>Asraful Haque</td>
+								<td>haq@gmail.com</td>
+								<td>01717700811</td>
+								<td><img src="assets/media/img/pp_photo/istockphoto-615279718-612x612.jpg" alt=""></td>
+								<td>
+									<a class="btn btn-sm btn-info" href="#">View</a>
+									<a class="btn btn-sm btn-warning" href="#">Edit</a>
+									<a class="btn btn-sm btn-danger" href="#">Delete</a>
+								</td>
+							</tr>
+							
+
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+		<div class="wrap-table shadow">
+		<div class="card">
+			<div class="card-body">
+				<h2>All Data</h2>
+				<table class="table table-striped">
+					<thead>
+						<tr>
+							<th>#</th>
+							<th>Name</th>
+							<th>Email</th>
+							<th>Cell</th>
+							<th>Photo</th>
+							<th>Action</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>1</td>
+							<td>Asraful Haque</td>
+							<td>haq@gmail.com</td>
+							<td>01717700811</td>
+							<td><img src="assets/media/img/pp_photo/istockphoto-615279718-612x612.jpg" alt=""></td>
+							<td>
+								<a class="btn btn-sm btn-info" href="#">View</a>
+								<a class="btn btn-sm btn-warning" href="#">Edit</a>
+								<a class="btn btn-sm btn-danger" href="#">Delete</a>
+							</td>
+						</tr>
+						<tr>
+							<td>1</td>
+							<td>Asraful Haque</td>
+							<td>haq@gmail.com</td>
+							<td>01717700811</td>
+							<td><img src="assets/media/img/pp_photo/istockphoto-615279718-612x612.jpg" alt=""></td>
+							<td>
+								<a class="btn btn-sm btn-info" href="#">View</a>
+								<a class="btn btn-sm btn-warning" href="#">Edit</a>
+								<a class="btn btn-sm btn-danger" href="#">Delete</a>
+							</td>
+						</tr>
+						<tr>
+							<td>1</td>
+							<td>Asraful Haque</td>
+							<td>haq@gmail.com</td>
+							<td>01717700811</td>
+							<td><img src="assets/media/img/pp_photo/istockphoto-615279718-612x612.jpg" alt=""></td>
+							<td>
+								<a class="btn btn-sm btn-info" href="#">View</a>
+								<a class="btn btn-sm btn-warning" href="#">Edit</a>
+								<a class="btn btn-sm btn-danger" href="#">Delete</a>
+							</td>
+						</tr>
+						<tr>
+							<td>1</td>
+							<td>Asraful Haque</td>
+							<td>haq@gmail.com</td>
+							<td>01717700811</td>
+							<td><img src="assets/media/img/pp_photo/istockphoto-615279718-612x612.jpg" alt=""></td>
+							<td>
+								<a class="btn btn-sm btn-info" href="#">View</a>
+								<a class="btn btn-sm btn-warning" href="#">Edit</a>
+								<a class="btn btn-sm btn-danger" href="#">Delete</a>
+							</td>
+						</tr>
+						<tr>
+							<td>1</td>
+							<td>Asraful Haque</td>
+							<td>haq@gmail.com</td>
+							<td>01717700811</td>
+							<td><img src="assets/media/img/pp_photo/istockphoto-615279718-612x612.jpg" alt=""></td>
+							<td>
+								<a class="btn btn-sm btn-info" href="#">View</a>
+								<a class="btn btn-sm btn-warning" href="#">Edit</a>
+								<a class="btn btn-sm btn-danger" href="#">Delete</a>
+							</td>
+						</tr>
+						
+
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</div>
+	
+
+	</div>
+
+	
+
+
+
+
+
+
+
+	<!-- JS FILES  -->
+	<script src="assets/js/jquery-3.4.1.min.js"></script>
+	<script src="assets/js/popper.min.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
+	<script src="assets/js/custom.js"></script>
+</body>
+</html>
---
+++ profile.php
@@ -0,0 +1,12 @@
+	
+
+	<div class="wrap shadow">
+		<div class="card">
+			<div class="card-body">
+				<h2>Your Profile</h2>
+				<img style="width:300px; height:300px; border-radius:50%; margin:40px auto;" src="assets/pp_photo/img1.jpeg" alt="">
+			
+			</div>
+		</div>
+	</div>
+	
